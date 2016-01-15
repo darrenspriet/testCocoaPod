385717717da4e839c9b28bf8210168ed58f78ce3
Darren Spriet <darren.spriet@pointclickcare.com>
2016-01-15 13:03:19 -0500

    Initial commit

---
+++ testCocoaPod/SCCSecureTextField.m
@@ -0,0 +1,57 @@
+#import "SCCSecureTextField.h"
+
+@implementation SCCSecureTextField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    
+    if (self)
+    {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if (self)
+    {
+        [self commonInit];
+    }
+    
+    return self;
+}
+
+//Darren - Disables the Menu when selecting text
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    return NO;
+}
+
+#pragma mark - Private
+
+- (void)commonInit
+{
+    self.autocorrectionType = UITextAutocorrectionTypeNo;
+    
+    [self removeCopyPasteFromKeyboardOptionsIfNeeded];
+}
+
+- (void)removeCopyPasteFromKeyboardOptionsIfNeeded
+{
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    {
+        if ([self respondsToSelector:@selector(inputAssistantItem)])
+        {
+            UITextInputAssistantItem* item = [self inputAssistantItem];
+            item.leadingBarButtonGroups = @[];
+            item.trailingBarButtonGroups = @[];
+        }
+    }
+}
+
+@end
---
+++ testCocoaPod/testCocoaPod.h
@@ -0,0 +1,21 @@
+//
+//  testCocoaPod.h
+//  testCocoaPod
+//
+//  Created by Darren Spriet on 2016-01-15.
+//  Copyright © 2016 Darren Spriet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testCocoaPod.
+FOUNDATION_EXPORT double testCocoaPodVersionNumber;
+
+//! Project version string for testCocoaPod.
+FOUNDATION_EXPORT const unsigned char testCocoaPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+#import <testCocoaPod/SCCSecureTextField.h>
+
+
